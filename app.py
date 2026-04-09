b18721c4e2d2101fbe2d0a7ab52c8f4803439bb5
Cheelo <112832809+CheeloHamududu@users.noreply.github.com>
2026-04-09 10:57:52 +0200

    changed PAYE based on 2026 Zambian tax bands

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ def calculate_napsa(gross_pay):
     return min(contribution, napsa_ceiling)
 
 def calculate_paye(taxable_income):
-    """Calculates PAYE based on the 2017 Zambian Tax Bands."""
+    """Calculates PAYE based on the 2026 Zambian Tax Bands."""
     tax = 0
-    if taxable_income <= 3300:
+    if taxable_income <= 5100:
         tax = 0
-    elif taxable_income <= 4100:
-        tax = (taxable_income - 3300) * 0.25
-    elif taxable_income <= 6200:
-        tax = (800 * 0.25) + (taxable_income - 4100) * 0.30
+    elif taxable_income <= 7100:
+        tax = (taxable_income - 5100) * 0.20
+    elif taxable_income <= 9200:
+        tax = (800 * 0.20) + (taxable_income - 7100) * 0.30
     else:
-        tax = (800 * 0.25) + (2100 * 0.30) + (taxable_income - 6200) * 0.375
+        tax = (800 * 0.25) + (2100 * 0.30) + (taxable_income - 9200) * 0.37
     return tax
 
 def main():
